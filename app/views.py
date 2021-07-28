187f846788f66db03e3a28563a7c60e333833ca7
mantasarul <mantasarul.elahi@northsouth.edu>
2021-07-28 21:16:58 +0600

    second commit

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
-from django.http.response import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView, View
-from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from .models import Account, AccountEmail, AccountPhone
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = 'home.html'
+
