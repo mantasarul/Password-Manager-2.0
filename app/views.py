b427d3ced8296feb91d5869b2a4d6819d7112796
mantasarul <mantasarul.elahi@northsouth.edu>
2021-07-28 20:23:30 +0600

    greeva added

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic.base import TemplateView, View
+from django.views.generic.edit import CreateView, DeleteView, FormView, UpdateView
+from django.views.generic.list import ListView
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = 'home.html'
